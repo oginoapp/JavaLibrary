146ef2fcfde64fb799b6cf940882da71fb105bd2
oginoapp <mailofogino@gmail.com>
2018-01-05 12:26:34 +0900

    containsメソッドの追加

--- src/util/ArrayUtility.java
+++ src/util/ArrayUtility.java
@@ -5,10 +5,10 @@ import java.lang.reflect.Array;
 public class ArrayUtility{
 
 	/**
-	 * @機能概要：配列の連結を行う
-	 * @引数１：配列１
-	 * @引数２：配列２
-	 * @戻り値：連結された配列
+	 * 配列の連結を行う
+	 * @param arr1 配列１
+	 * @param arr2 配列２
+	 * @return 連結された配列
 	 */
 	public static <T> T[] concat(T[] arr1, T[] arr2){
 		@SuppressWarnings("unchecked")
@@ -21,19 +21,41 @@ public class ArrayUtility{
 	}
 
 	/**
-	 * @機能概要：配列の要素数を1つ増やす
-	 * @引数１：配列
-	 * @戻り値：要素数を増やした配列
+	 * 配列の中に指定した要素が存在するかどうかを確認する
+	 * @param arr 配列
+	 * @param value 指定した要素
+	 * @return equalsを使って比較して存在した場合
+	 */
+	public static <T> boolean contains(T[] arr, T value){
+		boolean result = false;
+
+		for(T t : arr){
+			if(t == null){
+				continue;
+			}
+			if(t.equals(value)){
+				result = true;
+				break;
+			}
+		}
+
+		return result;
+	}
+
+	/**
+	 * 配列の要素数を1つ増やす
+	 * @param arr 配列
+	 * @return 要素数を増やした配列
 	 */
 	public static <T> T[] expansion(T[] arr){
 		return expansion(arr, 1);
 	}
 
 	/**
-	 * @機能概要：配列の要素数を増やす
-	 * @引数１：配列
-	 * @引数２：増やす要素数
-	 * @戻り値：要素数を増やした配列
+	 * 配列の要素数を増やす
+	 * @param arr 配列
+	 * @param increment 増やす要素数
+	 * @return 要素数を増やした配列
 	 */
 	public static <T> T[] expansion(T[] arr, int increment){
 		@SuppressWarnings("unchecked")
