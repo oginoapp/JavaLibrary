b864678c94abe2e3845a23b6f1ee33a73899c35c
oginoapp <mailofogino@gmail.com>
2018-02-10 11:40:52 +0900

    sql.DBConnetionをOracleにも対応

--- src/sql/DBConnection.java
+++ src/sql/DBConnection.java
@@ -6,29 +6,43 @@ import java.sql.SQLException;
 
 /**
  * DBとの接続を取得するクラス（シングルトン実装）
+ *
+ * ConnectionInfo info = new ConnectionInfo();
+ * info.connectionString = DBConnection.DEFAULT_CONNECTIONSTRING_MYSQL;
+ * info.username = "root";
+ * info.password = "";
+ * DBConnection.setConnectionInfo(info);
+ * Connection conn = DBConnection.getConnection(true);
  */
 public class DBConnection{
+
+	public static final String DEFAULT_CONNECTIONSTRING_MYSQL = "jdbc:mysql://localhost:3306/test"
+			+ "?useSSL=false"
+			+ "&requireSSL=false"
+			+ "&zeroDateTimeBehavior=convertToNull"
+			+ "&characterEncoding=utf8";
+
+	public static final String DEFAULT_CONNECTIONSTRING_ORACLE = "jdbc:oracle:thin:@localhost:1521:XE";
+
 	private static Connection conn = null;
 	private static long lastConnectTime = 0;
 	private static long reConnectInterval = 1000 * 60 * 60 * 1;
 	private static ConnectionInfo info = null;
 
-	public static class ConnectionInfo{
-		public String host = "localhost";
-		public int port = 3306;
-		public String database = null;
+	public static class ConnectionInfo {
+		public String connectionString = DEFAULT_CONNECTIONSTRING_MYSQL;
 		public String username = null;
 		public String password = null;
 	}
 
     /* プライベートコンストラクタ */
-	private DBConnection(){}
+	private DBConnection() { }
 
 	/**
 	 * DBの接続情報を設定する
 	 * @param info DBの接続情報
 	 */
-	public static void setConnectionInfo(ConnectionInfo info){
+	public static void setConnectionInfo(ConnectionInfo info) {
 		DBConnection.info = info;
 	}
 
@@ -39,10 +53,10 @@ public class DBConnection{
 	 * @throws SQLException
 	 * @throws ClassNotFoundException
 	 */
-	public static Connection getConnection() throws SQLException, ClassNotFoundException{
+	public static Connection getConnection() throws SQLException, ClassNotFoundException {
 		return getConnection(false);
 	}
-	public static Connection getConnection(boolean newConnection) throws SQLException, ClassNotFoundException{
+	public static Connection getConnection(boolean newConnection) throws SQLException, ClassNotFoundException {
 		if(System.currentTimeMillis() > lastConnectTime + reConnectInterval){
 			newConnection = true;
 		}
@@ -50,12 +64,7 @@ public class DBConnection{
 		if(conn == null || conn.isClosed() || newConnection){
 			Class.forName("com.mysql.jdbc.Driver");
 			Connection conn = DriverManager.getConnection(
-					"jdbc:mysql://" + info.host + ":" + info.port + "/" + info.database
-					+ "?useSSL=false"
-					+ "&requireSSL=false"
-					+ "&zeroDateTimeBehavior=convertToNull"
-					+ "&characterEncoding=utf8"
-					,info.username, info.password);
+					info.connectionString, info.username, info.password);
 			DBConnection.conn = conn;
             lastConnectTime = System.currentTimeMillis();
 		}
@@ -64,10 +73,11 @@ public class DBConnection{
 	}
 
 	/**
-	 * DBとの接続を閉じる
+	 * DBとの接続を明示的に閉じる
 	 * @throws SQLException
 	 */
-	public static void close() throws SQLException{
+	public static void close() throws SQLException {
 		if(conn != null) conn.close();
 	}
+
 }
