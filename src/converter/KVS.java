788a16f6a824e034e34787fd8c239e7c2aad23c9
oginoapp <mailofogino@gmail.com>
2018-03-02 22:12:58 +0900

    修正

--- src/converter/KVS.java
+++ src/converter/KVS.java
@@ -6,18 +6,28 @@ package converter;
 public class KVS {
 
 	/**
+	 * 値の種類
+	 */
+	public static enum ValueType {
+		Value,
+		String,
+		List,
+		Map
+	}
+
+	/**
 	 * サンプル
 	 */
 	public static void main(String[] args) {
-		KVS kvs = new KVS(null,
+		KVS kvs = new KVS(ValueType.Map,
 			new KVS("a1", "a1_data"),
-			new KVS("a2",
+			new KVS("a2", ValueType.List,
 				new KVS("b1", "b1_data"),
-				new KVS(null, "b2_data")
+				new KVS("data")
 			)
 		);
 
-		//{"a1":"a1_data","a2":{"b1":"b1_data","b2_data"}}
+		//{"a1":"a1_data","a2":[{"b1":"b1_data"},"data"]}
 		System.out.println(kvs.toJson());
 	}
 
@@ -25,14 +35,41 @@ public class KVS {
 	private String key;
 	/* 値 */
 	private Object value;
+	/* 値の種類 */
+	private ValueType type = ValueType.String;
+	/* 値のみであるか */
+	private boolean isValueOnly;
 
+	/**
+	 * 値のみ
+	 */
+	public KVS(String value) {
+		this(null, value);
+		isValueOnly = true;
+	}
+
+	/**
+	 * キーと値のペア
+	 */
 	public KVS(String key, String value) {
 		this.key = key;
 		this.value = value;
 	}
 
-	public KVS(String key, KVS... value) {
+	/**
+	 * 値がMapかListの場合
+	 */
+	public KVS(ValueType type, KVS... value) {
+		this(null, type, value);
+		isValueOnly = true;
+	}
+
+	/**
+	 * 値がMapかListの場合
+	 */
+	public KVS(String key, ValueType type, KVS... value) {
 		this.key = key;
+		this.type = type;
 		this.value = value;
 	}
 
@@ -42,28 +79,51 @@ public class KVS {
 	public String toJson() {
 		StringBuilder json = new StringBuilder();
 
-		if(key != null) {
+		if(!isValueOnly) {
 			json.append("\"");
 			json.append(escape(key));
 			json.append("\":");
 		}
 
-		if(value == null) {
-			json.append("null");
-		} else if(value instanceof String) {
-			json.append("\"");
+		switch(type) {
+		case Map:
+			json.append("{");
+			break;
+		case List:
+			json.append("[");
+			break;
+		default:
+			break;
+		}
+
+		if(value instanceof String) {
+			if(type == ValueType.String) json.append("\"");
 			json.append(escape(value.toString()));
-			json.append("\"");
+			if(type == ValueType.String) json.append("\"");
 		} else if(value instanceof KVS[]) {
 			KVS[] values = (KVS[])value;
 
-			json.append("{");
 			for(int i = 0; i < values.length; i++) {
+				boolean quot = type != ValueType.Map && !values[i].isValueOnly;
+				if(quot)
+					json.append("{");
 				json.append(values[i].toJson());
+				if(quot)
+					json.append("}");
 				if(i < values.length - 1)
 					json.append(",");
 			}
+		}
+
+		switch(type) {
+		case Map:
 			json.append("}");
+			break;
+		case List:
+			json.append("]");
+			break;
+		default:
+			break;
 		}
 
 		return json.toString();
@@ -73,14 +133,32 @@ public class KVS {
 	 * 値をエスケープ
 	 */
 	private String escape(String str) {
+		if(str == null) return str;
 		StringBuilder sb = new StringBuilder();
 
 		for(char c : str.toCharArray()) {
 			switch(c) {
 			case '"':
-				sb.append("\\\\\"");
+				sb.append("\\\"");
+				break;
+			case '\b':
+				sb.append("\\b");
+				break;
+			case '\f':
+				sb.append("\\f");
+				break;
+			case '\n':
+				sb.append("\\n");
+				break;
+			case '\r':
+				sb.append("\\r");
+				break;
+			case '\t':
+				sb.append("\\t");
+				break;
 			default:
 				sb.append(c);
+				break;
 			}
 		}
 
@@ -88,6 +166,4 @@ public class KVS {
 	}
 
 }
-
-
 
