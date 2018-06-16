ba86a5e6e24bfbe88912b063a1d7d0e913b3d941
oginoapp <mailofogino@gmail.com>
2018-06-16 17:41:46 +0900

    基数変換で62進数まで対応

--- src/converter/RadixConverter.java
+++ src/converter/RadixConverter.java
@@ -8,15 +8,27 @@ import java.math.RoundingMode;
  */
 public class RadixConverter {
 
+	/* [0-9] [a-z] [A-Z] の順に並ぶ */
+	private static final char[] RADIX_TABLE = new char[]{
+			'0','1','2','3','4','5','6','7','8','9',
+			'a','b','c','d','e','f','g','h','i','j',
+			'k','l','m','n','o','p','q','r','s','t',
+			'u','v','w','x','y','z',
+			'A','B','C','D','E','F','G','H','I','J',
+			'K','L','M','N','O','P','Q','R','S','T',
+			'U','V','W','X','Y','Z'
+	};
+
 	/**
-	 * n進数⇒n進数変換 (2 <= n <= 36)
+	 * n進数⇒n進数変換
 	 * 非常に大きな桁数と小数に対応
+	 * 62進数まで対応
 	 *
 	 * 例：
 	 * ("3.141592", 10, 16)   ⇒ 3.243f5f91600f345069a4df47f993d5347...
 	 * ("3.141592", 10, 2)    ⇒ 11.00100100001111110101111110010001...
 	 * ("11111111.11", 2, 10) ⇒ 255.75
-	 * ("ABCD.EFGH", 32, 16)  ⇒ 52d8d.73e11
+	 * ("abcd.efgh", 32, 16)  ⇒ 52d8d.73e11
 	 *
 	 * @param anyDecimal 変換したい数
 	 * @param 基数（変換元）
@@ -58,7 +70,7 @@ public class RadixConverter {
 			// 計算
 			char[] numberPart = chunk[0].toCharArray();
 			for (int i = numberPart.length - 1, exponent = 0; i >= 0; i--, exponent++) {
-				BigDecimal num1 = new BigDecimal(Integer.parseInt(String.valueOf(numberPart[i]), radix));
+				BigDecimal num1 = new BigDecimal(integerParseInt(String.valueOf(numberPart[i]), radix));
 				BigDecimal num2 = bdRadix.pow(exponent);
 				decimal = decimal.add(num1.multiply(num2));
 			}
@@ -72,7 +84,7 @@ public class RadixConverter {
 			// 計算
 			char[] fractionalPart = chunk[1].toCharArray();
 			for (int i = 0, exponent = 1; i < fractionalPart.length; i++, exponent++) {
-				BigDecimal num1 = new BigDecimal(Integer.parseInt(String.valueOf(fractionalPart[i]), radix));
+				BigDecimal num1 = new BigDecimal(integerParseInt(String.valueOf(fractionalPart[i]), radix));
 				BigDecimal num2 = bdRadix.pow(exponent);
 				decimal = decimal.add(num1.divide(num2, 1024, RoundingMode.HALF_UP));
 			}
@@ -82,23 +94,23 @@ public class RadixConverter {
 	}
 
 	/**
-	 * 10進数⇒n進数 (2 <= n <= 36)
+	 * 10進数⇒n進数
 	 *
 	 * @param decimal 10進数
 	 * @param radix 基数
-	 * @param n進数文字列
+	 * @return n進数文字列
 	 */
 	public static String decimalToAnyDecimal(BigDecimal decimal, int radix) {
 		return decimalToAnyDecimal(decimal, radix, 1024);
 	}
 
 	/**
-	 * 10進数⇒n進数 (2 <= n <= 36)
+	 * 10進数⇒n進数
 	 *
 	 * @param decimal 10進数
 	 * @param radix 基数
 	 * @param scale 小数点以下の桁数
-	 * @param n進数文字列
+	 * @return n進数文字列
 	 */
 	public static String decimalToAnyDecimal(BigDecimal decimal, int radix, int scale) {
 		if (decimal == null)
@@ -124,10 +136,10 @@ public class RadixConverter {
 			mod = numberPart.remainder(base);
 			numberPart = numberPart.divide(base).setScale(0, BigDecimal.ROUND_DOWN);
 
-			result = result.insert(0, Integer.toString(Integer.parseInt(mod.toString()), radix));
+			result = result.insert(0, integerToString(Integer.parseInt(mod.toString()), radix));
 		}
 		mod = numberPart.remainder(base);
-		result = result.insert(0, Integer.toString(Integer.parseInt(mod.toString()), radix));
+		result = result.insert(0, integerToString(Integer.parseInt(mod.toString()), radix));
 
 		// 小数部分
 		BigDecimal fractionalPart = decimal.remainder(BigDecimal.ONE);
@@ -144,12 +156,62 @@ public class RadixConverter {
 
 			// 小数点以下を切り捨てた値を取得
 			int n = Integer.parseInt(fractionalPart.setScale(0, BigDecimal.ROUND_DOWN).toString());
-			result.append(Integer.toString(n, radix));
+			result.append(integerToString(n, radix));
 		}
 
 		if (negative) result.insert(0, "-");
 
 		return result.toString();
+	}
+
+	/**
+	 * int型数値⇒n進数文字列
+	 * @param num int型数値
+	 */
+	private static String integerToString(int num, int radix){
+		if (radix > RADIX_TABLE.length)
+			throw new IllegalArgumentException("radix > RADIX_TABLE.length");
+
+		StringBuilder result = new StringBuilder();
+
+		int tmp = num;
+		int mod = 0;
+		while(tmp >= radix){
+			mod = tmp % radix;
+			tmp = tmp / radix;
+
+			result = result.insert(0, RADIX_TABLE[(int)mod]);
+		}
+		mod = tmp % radix;
+		result = result.insert(0, RADIX_TABLE[(int)mod]);
+
+		return result.toString();
+	}
+
+	/**
+	 * n進数文字列⇒int型の数値
+	 * @param str n進数文字列
+	 */
+	private static long integerParseInt(String str, int radix){
+		if (radix > RADIX_TABLE.length)
+			throw new IllegalArgumentException("radix > RADIX_TABLE.length");
+
+		int result = 0;
+
+		char[] chars = str.toCharArray();
+		int power = 1;
+		for(int i = chars.length - 1; i >= 0; i--, power *= radix){
+			int base = -1;
+			for(int j = 0; i < radix; j++){
+				if(chars[i] == RADIX_TABLE[j]){
+					base = j;
+					break;
+				}
+			}
+			result += base * power;
+		}
+
+		return result;
 	}
 
 	/**
@@ -159,7 +221,7 @@ public class RadixConverter {
 		// 許容誤差
 		BigDecimal accuracy = new BigDecimal(0.000000000000001);
 
-		// 2進数でのテスト
+		// 2進数での誤差テスト
 		for (double i = 0; i < 100; i += 0.1) {
 			BigDecimal decimal = new BigDecimal(i);
 			String anyDecimal = RadixConverter.decimalToAnyDecimal(decimal, 2);
@@ -169,7 +231,7 @@ public class RadixConverter {
 			}
 		}
 
-		// 8進数でのテスト
+		// 8進数での誤差テスト
 		for (double i = 0; i < 100; i += 0.1) {
 			BigDecimal decimal = new BigDecimal(i);
 			String anyDecimal = RadixConverter.decimalToAnyDecimal(decimal, 8);
@@ -179,7 +241,7 @@ public class RadixConverter {
 			}
 		}
 
-		// 16進数でのテスト
+		// 16進数での誤差テスト
 		for (double i = 0; i < 100; i += 0.1) {
 			BigDecimal decimal = new BigDecimal(i);
 			String anyDecimal = RadixConverter.decimalToAnyDecimal(decimal, 16);
@@ -188,6 +250,31 @@ public class RadixConverter {
 				throw new ArithmeticException();
 			}
 		}
+
+		// 変換が正しいかどうかのテスト
+		String str = null;
+		System.out.println(str = RadixConverter.anyDecimalToAnyDecimal("3.141592", 10, 16));
+		if (!str.startsWith("3.243f5f91600f345069a4df47f993d5347")) {
+			throw new AssertionError();
+		}
+		System.out.println(str = RadixConverter.anyDecimalToAnyDecimal("3.141592", 10, 2));
+		if (!str.startsWith("11.00100100001111110101111110010001")) {
+			throw new AssertionError();
+		}
+		System.out.println(str = RadixConverter.anyDecimalToAnyDecimal("11111111.11", 2, 10));
+		if (!str.equals("255.75")) {
+			throw new AssertionError();
+		}
+		System.out.println(str = RadixConverter.anyDecimalToAnyDecimal("abcd.efgh", 32, 16));
+		if (!str.equals("52d8d.73e11")) {
+			throw new AssertionError();
+		}
+		System.out.println(str = RadixConverter.anyDecimalToAnyDecimal("xyz.XYZ", 62, 10));
+		if (!str.startsWith("128995.96747759390419925480849921117115")) {
+			throw new AssertionError();
+		}
+
+		System.out.println("テスト完了");
 	}
 
 }
